10f749dd89d10a73d93aab1d6fbd7c3851d47856
altumspatium-droid <altumspatium@gmail.com>
2026-07-02 00:55:14 +0200

    Refactor feeding calculation return values

--- calculator.py
+++ calculator.py
@@ -324,10 +324,9 @@ def eaten_to_table(eaten):
 def calculate_feeding(limits, maxes):
     m = FinalMount(limits, maxes)
     m.fully_eat(mats_lists)
-    return len(m.eaten)
-    # return eaten_to_table(m.eaten)
+    return eaten_to_table(m.eaten)
 
 def calculate_num_needed(limits, maxes):
     m = FinalMount(limits, maxes)
     m.fully_eat(mats_lists)
-    return m.eaten
+    return len(m.eaten)
